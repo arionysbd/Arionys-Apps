283bc532ee0bd37443f73283c41f513ccc70f4d8
Md Nur Ahmad <234045679+arionysbd@users.noreply.github.com>
2025-09-28 01:14:39 +0600

    chat

---
+++ Arionys_Chat/src/app/calls/page.tsx
@@ -0,0 +1,33 @@
+// 'use client';
+
+// import { NavigationBar } from '@/components/navigation-bar';
+// import { NewGroup } from '@/components/new-group';
+// import { CallContent } from '@/components/call-content';
+
+// const Calls = () => {
+//   return (
+//     <>
+//       <NavigationBar trigger={<NewGroup />} />
+//       <CallContent />
+//     </>
+//   );
+// };
+
+// export default Calls;
+
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+const Calls = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('https://meet.arionys.com/');
+  }, [router]);
+
+  return null; // Prevents rendering anything before redirect
+};
+
+export default Calls;
+
